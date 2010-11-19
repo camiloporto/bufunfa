ba2f6d7599eaab6cc23f563219eda3458a882bf2
Camilo Porto <camiloporto@gmail.com>
2010-11-18 22:34:16 -0300

    adicionado a interface do servico de sistema de contas

---
+++ src/main/java/br/com/bufunfa/finance/conta/ISistemaContaService.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.bufunfa.finance.conta;
+
+/**
+ * Interface do servico de sistema de contas
+ * 
+ * @author camilo
+ *
+ */
+public interface ISistemaContaService {
+	
+	/**
+	 * Adiciona um novo sistema de contas
+	 * @param nomeSistema nome do sistema de contas
+	 * @TODO Acrescentar/alterar os parametros necessarios para se criar um sistema de contas?
+	 */
+	public void addSistemaConta(String nomeSistema);
+
+}
